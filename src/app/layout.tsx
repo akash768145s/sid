d4c40cce116262b963d05daae0f6ebffa3bc114c
akash768145s <akash2210476@ssn.edu.in>
2024-07-20 18:14:57 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
+// src/app/layout.js
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import "@uploadthing/react/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { getServerSession } from "next-auth";
 import SessionProvider from "@/utils/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,17 +13,17 @@ export const metadata: Metadata = {
   description: "Sell IT DUDE",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>
-          <div className="mx-auto  text-2xl gap-2 mb-10">
+        <SessionProvider>
+          <Navbar />
+          <div className="mx-auto text-2xl gap-2 mb-10">
             {children}
           </div>
         </SessionProvider>
